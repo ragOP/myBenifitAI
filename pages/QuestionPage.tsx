d1223598603afb2225e6cedb7a48de9765574b3c
ragOP <najmiraghib@gmail.com>
2025-05-25 01:19:14 +0530

    chanages

--- pages/QuestionPage.tsx
+++ pages/QuestionPage.tsx
@@ -409,8 +409,7 @@ const styles = StyleSheet.create({
   inner: {
     flex: 1,
     justifyContent: 'center',
-    paddingBottom: 60,
-    marginTop: 100,
+paddingTop: 220,
   },
   questionText: {
     color: '#fff',
